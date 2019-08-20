12aef0a97bda107b380f14ad8b98be0b1bd9dbb7
rahkaroline <51764263+rahkaroline@users.noreply.github.com>
2019-08-20 14:28:47 -0300

    commit

---
+++ contas/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Transacao
+import datetime
+
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
