7345a444c6d3f59e83a7464ef428f0605bf88373
rahkaroline <51764263+rahkaroline@users.noreply.github.com>
2019-08-21 11:47:41 -0300

    MeuProjeto

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Transacao
+from .form import TransacaoForm
 import datetime
 
 
@@ -12,7 +13,15 @@ def home(request):
 
     return render(request, 'contas/home.html', data)
 
+
 def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request, 'contas/listagem.html', data)
+
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
